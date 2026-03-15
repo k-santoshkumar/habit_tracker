22785cb46cf4f83d31fd6327254110bd6e579690
Santosh Kumar Kurichetti <ksk@Santoshs-MacBook-Air.local>
2026-03-15 13:06:43 +0530

    updating sh file

--- backend.sh
+++ backend.sh
@@ -2,10 +2,7 @@
 set -e
 
 echo "Starting Health Tracker Backend..."
-source backend/venv/bin/activate
-export PYTHONPATH=$PWD
 python backend/main.py &
 BACKEND_PID=$!
-
 echo "Backend: http://localhost:8000"
 
